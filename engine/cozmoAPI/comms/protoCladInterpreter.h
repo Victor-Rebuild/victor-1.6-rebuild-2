5e0e5e2f2cb2380a3ab4740e658a465d53381b21
Bruce von Kugelgen <45771625+BruceVonK@users.noreply.github.com>
2019-04-23 16:30:47 -0700

    VIC-12583 SDK camera settings (#5531)
    * Now handles just setting auto-exposure (where other message parameters may be zero)
    * more changes required for improved SDK handling of camera settings
    * sends camera settings events to SDK

--- engine/cozmoAPI/comms/protoCladInterpreter.h
+++ engine/cozmoAPI/comms/protoCladInterpreter.h
@@ -65,6 +65,10 @@ private:
       const external_interface::GatewayWrapper& proto_message,
       ExternalInterface::MessageGameToEngine& clad_message);
 
+  static void ProtoCameraConfigRequestToClad(
+      const external_interface::GatewayWrapper& proto_message,
+      ExternalInterface::MessageGameToEngine& clad_message);
+
   //
   // Clad-to-Proto interpreters
   //
@@ -94,6 +98,14 @@ private:
 
   static void CladEndOfMessageToProto(
       const ExternalInterface::MessageEngineToGame& clad_message, 
+      external_interface::GatewayWrapper& proto_message);
+
+  static void CladPerRobotSettingsToProto(
+      const ExternalInterface::MessageEngineToGame& clad_message,
+      external_interface::GatewayWrapper& proto_message);
+
+  static void CladCurrentCameraParamsToProto(
+      const ExternalInterface::MessageEngineToGame& clad_message,
       external_interface::GatewayWrapper& proto_message);
 };
 
