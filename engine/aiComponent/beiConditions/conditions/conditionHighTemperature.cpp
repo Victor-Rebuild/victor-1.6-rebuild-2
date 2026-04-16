e162a19fb16e565ed0a9d5a43aa97f20f057fe72
Switch-modder <deolrajjyot@gmail.com>
2026-04-16 16:04:47 -0400

    Checkpoint here for now, come back later

--- engine/aiComponent/beiConditions/conditions/conditionHighTemperature.cpp
+++ engine/aiComponent/beiConditions/conditions/conditionHighTemperature.cpp
@@ -33,7 +33,7 @@ namespace
   // from 60C that there's some margin for the battery to heat
   // up further while he's frantically searching for and
   // docking to the charger.
-  const int kBatteryOverheatingThreshold_degC = 55;
+  //const int kBatteryOverheatingThreshold_degC = 55;
 
   const bool skipOverheatCheck = IsXray();
 }
@@ -46,7 +46,7 @@ ConditionHighTemperature::ConditionHighTemperature(const Json::Value& config)
 bool ConditionHighTemperature::AreConditionsMetInternal(BehaviorExternalInterface& bei) const
 {
   const bool hotCPU     = bei.GetRobotInfo().GetCpuTemperature_degC() >= kCPUOverheatingThreshold_degC;
-  const bool hotBattery = bei.GetRobotInfo().GetBatteryComponent().GetBatteryTemperature_C() >= kBatteryOverheatingThreshold_degC;
+  const bool hotBattery = true; // bei.GetRobotInfo().GetBatteryComponent().GetBatteryTemperature_C() >= kBatteryOverheatingThreshold_degC;
   return hotCPU || (hotBattery && !skipOverheatCheck);
 }
 
