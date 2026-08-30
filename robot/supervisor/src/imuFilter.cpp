e8da0765b12a91e17a873a04d7929610dee71612
Kerigan Creighton <kerigancreighton@gmail.com>
2026-08-30 13:29:23 -0500

    a few things: * more closely match Anki retransmit behavior * send the whole outstanding window on loss, up to 3 frames * fast retransmit * audio and screen are now unreliable packets, so they don't retransmit (giving priority to motors and other stuff) * don't build WeBots stuff if ./sim.sh -cozmo

--- robot/supervisor/src/imuFilter.cpp
+++ robot/supervisor/src/imuFilter.cpp
@@ -29,6 +29,7 @@
 #include "anki/cozmo/robot/logging.h"
 #include "anki/cozmo/robot/hal.h"
 #include "anki/cozmo/robot/DAS.h"
+#include "anki/cozmo/shared/cozmoConfig.h"
 
 #include "clad/robotInterface/messageRobotToEngine_send_helper.h"
 #include "clad/types/motorTypes.h"
@@ -688,7 +689,8 @@ namespace Anki {
             ++numCliffSensorsDetectingChange;
           }
         }
-        bool cliffBasedPickupDetect = numCliffSensorsDetectingChange >= NUM_CLIFF_SENSORS_FOR_CHANGE_DETECT_PICKUP;
+        bool cliffBasedPickupDetect = !IsCozmoBody() &&
+                                      numCliffSensorsDetectingChange >= NUM_CLIFF_SENSORS_FOR_CHANGE_DETECT_PICKUP;
 
 
         if (IsPickedUp()) {
