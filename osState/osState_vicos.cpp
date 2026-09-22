35573fc57d4c187602559c53508ae0f16b7fb8aa
Kerigan Creighton <kerigancreighton@gmail.com>
2026-09-21 19:59:44 -0500

    make temperature not report as being hotter than the surface of the sun on 4.9

--- osState/osState_vicos.cpp
+++ osState/osState_vicos.cpp
@@ -366,6 +366,9 @@ void OSState::UpdateTemperature_C() const
   _temperatureFile.open(kTemperatureFile, std::ifstream::in);
   if (_temperatureFile.is_open()) {
     _temperatureFile >> _cpuTemp_C;
+    if (_cpuTemp_C > 999) {
+      _cpuTemp_C /= 1000;
+    }
     _temperatureFile.close();
   }
   else {
